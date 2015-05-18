e2483540f7416e80eaf87fd2d46a8c410bcfe30f
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2015-05-18 13:04:01 +0300

    Events, inputs, ...

--- server.py
+++ server.py
@@ -32,6 +32,14 @@ class Device(Base):
     device_secret = Column(String, nullable=False)
 
     measurements = relationship('Measurement', backref='device')
+    inputs = relationship('Input', backref='device')
+
+
+class Input(Base):
+    __tablename__ = 'input'
+    id = Column(Integer, primary_key=True)
+    device_id = Column(Integer, ForeignKey('device.id'))
+    name = Column(String, nullable=False)
 
 
 class Measurement(Base):
@@ -54,7 +62,26 @@ class ViewHandler(tornado.web.RequestHandler):
     def get(self):
         device_ids = self.request.arguments.get('id', [])
         devices = session.query(Device).filter(Device.id.in_(device_ids)).all()
-        self.render("view_devices.html")
+        inputs = {
+            i[0] for i in
+            (
+                session.query(Input.name)
+                .filter(
+                    Input.device_id.in_(device_ids)
+                )
+                .order_by(Input.device_id, Input.id)
+                .all()
+            )
+        }
+        events = (
+            session.query(Measurement)
+            .filter(
+                Measurement.measurement_type=='event',
+                Measurement.device_id.in_(device_ids)
+            )
+            .order_by(Measurement.timestamp)
+        ).all()
+        self.render("view_devices.html", inputs=inputs, events=events)
 
 
 application = tornado.web.Application([
@@ -77,6 +104,14 @@ if __name__ == "__main__":
                 device_secret='secret %d' % i
             )
             session.add(device)
+            session.add(Measurement(
+                device=device,
+                ip_address='127.0.0.1',
+                measurement_name='Bootup',
+                measurement_type='event',
+                measurement_value='1',
+                timestamp=datetime.now() - timedelta(minutes=15*101)
+            ))
             for name in ['temperature_in', 'temperature_out']:
                 for m in range(100):
                     session.add(Measurement(
@@ -87,7 +122,28 @@ if __name__ == "__main__":
                         measurement_value=m,
                         timestamp=datetime.now() - timedelta(minutes=15*m)
                     ))
-
+            for m in range(10):
+                session.add(Measurement(
+                    device=device,
+                    ip_address='127.0.0.1',
+                    measurement_name='Door sensor 1',
+                    measurement_type='event',
+                    measurement_value='1',
+                    timestamp=datetime.now() - timedelta(minutes=15*m)
+                ))
+                session.add(Measurement(
+                    device=device,
+                    ip_address='127.0.0.1',
+                    measurement_name='Door sensor 1',
+                    measurement_type='event',
+                    measurement_value='0',
+                    timestamp=datetime.now() - timedelta(minutes=15*m) + timedelta(seconds=10)
+                ))
+            for name in ['Heating', 'Sauna', 'Alarm system', 'Unused']:
+                session.add(Input(
+                    device=device,
+                    name=name
+                ))
         session.commit()
 
     else:
